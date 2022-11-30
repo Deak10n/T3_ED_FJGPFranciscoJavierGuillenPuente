46c4834b6f128f8da8b1edb0cd9c96942b72e6cf
Fran_Guillen <fjavierguillenpuente@gmail.com>
2022-11-30 20:34:20 +0100

    Programa java.c

--- pro_FJGP_Fran_Guillen/src/pro_FJGP_Fran_Guillen/Hola_Mundo.java
+++ pro_FJGP_Fran_Guillen/src/pro_FJGP_Fran_Guillen/Hola_Mundo.java
@@ -3,7 +3,7 @@ package pro_FJGP_Fran_Guillen;
 public class Hola_Mundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("¡Hola mundo soy “Fran Guillén!");
 
 	}
 
